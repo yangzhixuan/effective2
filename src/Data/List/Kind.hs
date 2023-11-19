71f4a558bce94662f84de5c25f9986b61065ff89
Nicolas Wu <nicolas.wu@gmail.com>
2023-11-19 23:48:50 +0000

    Add Insert and Union for list kinds

--- src/Data/List/Kind.hs
+++ src/Data/List/Kind.hs
@@ -2,10 +2,20 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Data.List.Kind where
 
 type family (xs :: [k]) :++ (ys :: [k]) :: [k] where
-  (x ': xs) :++ ys = x ': (xs :++ ys)
   '[]       :++ ys = ys
+  (x ': xs) :++ ys = x ': (xs :++ ys)
 
+type family Insert (x :: k) (ys :: [k]) where
+  Insert x '[]       = '[x]
+  Insert x (x ': ys) = x ': ys
+  Insert x (y ': ys) = y ': Insert x ys
+
+
+type family Union (xs :: [k]) (ys :: [k]) :: [k] where
+  Union '[]       ys = ys
+  Union (x ': xs) ys = Insert x (Union xs ys)
