9ecc51a5e06a7d0e4039ab28e5773abe87d1deef
Nicolas Wu <nicolas.wu@gmail.com>
2023-11-20 00:06:22 +0000

    Add >> to Graded

--- src/Control/Monad/Graded.hs
+++ src/Control/Monad/Graded.hs
@@ -5,6 +5,7 @@
 module Control.Monad.Graded (GradedMonad(..)) where
 
 import Data.Kind (Type, Constraint)
+import Prelude hiding ((>>=))
 
 class GradedMonad (m :: k -> Type -> Type) where
   type Unit m :: k
@@ -12,4 +13,6 @@ class GradedMonad (m :: k -> Type -> Type) where
   type Inv  m (i :: k) (j :: k) :: Constraint
 
   (>>=) :: Inv m i j => m i a -> (a -> m j b) -> m (Plus m i j) b
+  (>>)  :: Inv m i j => m i a -> m j b -> m (Plus m i j) b
+  x >> y = x >>= const y
   return :: a -> m (Unit m) a
