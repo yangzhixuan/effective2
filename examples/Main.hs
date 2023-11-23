9a840cda35b5d8f52ce4877f65436a21d50125be
Nicolas Wu <nicolas.wu@gmail.com>
2023-11-23 23:41:25 +0000

    Test State

--- examples/Main.hs
+++ examples/Main.hs
@@ -6,9 +6,11 @@ import Hedgehog.Internal.TH
 
 import qualified Error
 import qualified Nondet
+import qualified State
 main :: IO ()
 main = defaultMain $ fmap checkParallel
   [ Error.examples
   , Nondet.examples
+  , State.examples
   ]
 
--- examples/State.hs
+++ examples/State.hs
@@ -1,87 +1,106 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE QualifiedDo #-}
 
 module State where
+
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Hedgehog.Internal.TH
 
 import Control.Effect
 import Control.Effect.State
 import Control.Effect.Catch
 import Control.Effect.Throw
 import Control.Effect.Except
-import Control.Monad (replicateM)
+import Control.Monad (replicateM, replicateM_)
 import Control.Monad.Trans.Maybe
 import qualified Control.Monad.Trans.State.Lazy as S
 
+incr :: Members [Get Int, Put Int] sig => Prog sig ()
+incr = do
+  x <- get
+  put @Int (x + 1)
 
-exampleState :: (Int, ())
-exampleState = handle (state 41) incr
--- ghci> exampleState
--- (42,())
+example_incr = property $ do
+  n <- forAll $ Gen.int $ Range.linear 1 1000
+  handle (state n) incr === (n+1,())
 
-catchDecr
-  :: Members [Put Int, Get Int, Throw, Catch] sig
-  => Prog sig ()
+-- decr :: Prog' '[Get Int, Put Int] ()
+decr :: Members [Get Int, Put Int] sig => Prog sig ()
+decr = do
+  x <- get
+  if x > 0
+    then put @Int (x - 1)
+    else return ()
+
+
+example_decr = property $ do
+  n <- forAll $ Gen.int $ Range.linear 1 1000
+  handle (state n) decr === (n- 1,())
+
+example_incrDecr = property $ do
+  n <- forAll $ Gen.int $ Range.linear 0 1000
+  (handle (state n) (do incr; decr) :: (Int, ())) === (n, ())
+
+catchDecr :: Members [Get Int, Put Int, Throw, Catch] sig => Prog sig ()
 catchDecr = do
   decr
-  catch (decr >> decr)
-        (return ())
+  catch
+    (do decr
+        decr)
+    (return ())
 
+catchDecr' = catchDecr @[Get Int, Put Int, Throw, Catch]
 
 globalState
-  :: s -> Handler [Throw, Catch, Put s, Get s, Local s]
+  :: s -> Handler '[Throw, Catch, Put s, Get s, Local s]
           [MaybeT,  S.StateT s]
-          [((,) s), Maybe] '[]
+          [(,) s, Maybe] '[]
 globalState s = except <&> state s
 
-exampleGlobalState :: (Int, Maybe ())
-exampleGlobalState = handle (globalState 2) catchDecr
 -- This is global state because the `Int` is decremented
 -- twice before the exception is thrown.
---
--- ghci> exampleGlobalState
--- (0,Just ())
-
+example_GlobalState = property $
+    (handle (globalState 2) catchDecr :: (Int, Maybe ()))
+  ===
+    (0,Just ())
 
 localState
   :: s -> Handler
-          [Put s, Get s, Local s, Throw, Catch]
-          [S.StateT s, MaybeT]
-          [Maybe, ((,) s)]
+          '[Put s, Get s, Local s, Throw, Catch]
+          '[S.StateT s, MaybeT]
+          '[Maybe, ((,) s)]
           '[]
 localState s = state s <&> except
 
-exampleLocalState :: Maybe (Int, ())
-exampleLocalState = handle (localState 2) catchDecr
 -- With local state, the state is reset to its value
 -- before the catch where the exception was raised.
---
--- ghci> exampleLocalState
--- Just (1,())
+example_localState = property $
+    (handle (localState 2) catchDecr :: Maybe (Int, ()))
+  ===
+    Just (1, ())
 
-catchDecr'
-  :: Members [Put Int, Get Int, Throw, Catch] sig
-  => Prog sig ()
-catchDecr' = do
+example_localState' = property $
+    (handle (localState 2) catchDecr :: Maybe (Int, ()))
+  ===
+    Just (1, ())
+
+
+catchDecr44 :: Members [Get Int, Put Int, Throw, Catch] sig => Prog sig ()
+catchDecr44 = do
   decr
-  catch (decr >> decr)
-        (replicateM 44 incr >> return ())
+  catch
+    (do decr
+        decr)
+    (do replicateM_ 44 incr)
 
 -- For instance you might want to allocate a bit more memory ...
 -- and a bit more ... and so on.
-exampleRetry1 :: (Int, Maybe ())
-exampleRetry1 = handle (retry <&> state 2) catchDecr'
+example_Retry1 = property $
+    (handle (retry <&> state 2) catchDecr44 :: (Int, Maybe ()))
+  ===
+    (42,Just ())
 
-
-incr
-  :: Members [Put Int, Get Int] sig
-  => Prog sig ()
-incr = do x <- get
-          put @Int (x + 1)
-
-decr
-  :: Members [Put Int, Get Int, Throw] sig
-  => Prog sig ()
-decr = do x <- get
-          if (x > 0)
-            then put @Int (x - 1)
-            else throw
-
+examples = $$(discoverPrefix "example_")
