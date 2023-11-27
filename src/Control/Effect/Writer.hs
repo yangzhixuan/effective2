03a74c9fd8457e6345139b5ae17249ae9037ae39
Nicolas Wu <nicolas.wu@gmail.com>
2023-11-27 11:53:00 +0000

    Implement the writer handler

--- src/Control/Effect/Writer.hs
+++ src/Control/Effect/Writer.hs
@@ -4,6 +4,7 @@
 module Control.Effect.Writer where
 import Control.Effect
 import Data.Tuple (swap)
+import Control.Monad.Trans.Class (MonadTrans, lift)
 import qualified Control.Monad.Trans.Writer as W
 import Data.HFunctor ( HFunctor(..) )
 
@@ -18,16 +19,21 @@ tell :: (Member (Tell w) effs, Monoid w) => w -> Prog effs ()
 tell w = (Call . inj) (Alg (Tell w (return ())))
 
 writerAlg
-  :: Monad m
+  :: (Monad m, Monoid w)
   => (forall x. oeff m x -> m x)
   -> (forall x.  Effs '[Tell w] (W.WriterT w m) x -> W.WriterT w m x)
-writerAlg oalg = undefined
+writerAlg oalg eff
+  | Just (Alg (Tell w k)) <- prj eff =
+      do W.tell w
+         return k
 
 writerFwd
-  :: Monad m
+  :: (Monad m, Monoid w)
   => (forall x. Effs sig m x -> m x)
   -> (forall x. Effs sig (W.WriterT w m) x -> W.WriterT w m x)
-writerFwd alg = undefined
+writerFwd alg (Eff (Alg x)) = lift (alg (Eff (Alg x)))
+writerFwd alg (Eff (Scp x)) = W.WriterT (alg (Eff (Scp (fmap W.runWriterT x))))
+writerFwd alg (Effs effs)   = writerFwd (alg . Effs) effs
 
 writer :: Monoid w => Handler '[Tell w] '[W.WriterT w] '[(,) w] '[] 
 writer = handler (fmap swap . W.runWriterT) writerAlg writerFwd
