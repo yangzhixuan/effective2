1a7419d48a4f075f526c1ba2612b4720e790e9ed
Nicolas Wu <nicolas.wu@gmail.com>
2024-06-06 11:51:29 +0100

    Test only new framework

--- docs/Main.hs
+++ docs/Main.hs
@@ -4,15 +4,15 @@ import Hedgehog
 import Hedgehog.Main
 
 import Error
-import Nondet
-import State
-import Parser
-import Graded ()
+-- import Nondet
+-- import State
+-- import Parser
+-- import Graded ()
 
 main :: IO ()
 main = defaultMain $ fmap checkParallel
   [ Error.examples
-  , Nondet.examples
-  , State.examples
-  , Parser.examples
+--  , Nondet.examples
+--  , State.examples
+--  , Parser.examples
   ]
