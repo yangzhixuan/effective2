c978da25519ba6c714f68376d3363ea474f51810
Nicolas Wu <nicolas.wu@gmail.com>
2024-07-22 16:33:59 +0100

    Inlining

--- src/Data/HFunctor/HCompose.hs
+++ src/Data/HFunctor/HCompose.hs
@@ -10,29 +10,35 @@ newtype HCompose (h :: (Type -> Type) -> (Type -> Type))
   = HCompose { getHCompose :: h (k f) a }
 
 instance Functor (h (k m)) => Functor (HCompose h k m) where
+    {-# INLINE fmap #-}
     fmap :: (a -> b) -> HCompose h k m a -> HCompose h k m b
     fmap f (HCompose x) = HCompose (fmap f x)
 
 instance (Applicative (h (k f)), Applicative f) =>
   Applicative (HCompose h k f) where
+    {-# INLINE pure #-}
     pure :: a -> HCompose h k f a
     pure x = HCompose (pure x)
 
+    {-# INLINE (<*>) #-}
     (<*>) :: HCompose h k f (a -> b) -> HCompose h k f a -> HCompose h k f b
     HCompose mf <*> HCompose mx = HCompose (mf <*> mx)
 
 instance (MonadTrans t1, MonadTrans t2, Monad m) =>
   Monad (HCompose t1 t2 m) where
+    {-# INLINE (>>=) #-}
     (>>=) :: HCompose t1 t2 m a -> (a -> HCompose t1 t2 m b) -> HCompose t1 t2 m b
     HCompose mx >>= f = HCompose (mx >>= getHCompose . f)
 
 instance (MonadTrans t1, MonadTrans t2) =>
   MonadTrans (HCompose t1 t2) where
+    {-# INLINE lift #-}
     lift :: Monad m => m a -> HCompose t1 t2 m a
     lift x = HCompose (lift (lift x))
 
 instance (HFunctor h, HFunctor k) =>
   HFunctor (HCompose h k) where
+    {-# INLINE hmap #-}
     hmap :: (HFunctor h, HFunctor k, Functor f, Functor g) =>
       (forall x. f x -> g x) -> HCompose h k f a -> HCompose h k g a
     hmap h (HCompose x) = HCompose (hmap (hmap h) x)
--- src/Data/HFunctor/HComposes.hs
+++ src/Data/HFunctor/HComposes.hs
@@ -26,15 +26,19 @@ data HComps hs f a where
   -- HCons :: h (HComps hs f) a -> HComps (h ': hs) f a
   HCons :: Functor (h (HComps hs f)) => h (HComps hs f) a -> HComps (h ': hs) f a
 
+{-# INLINE unHNil #-}
 unHNil  :: HComps '[] f a -> f a
 unHNil (HNil x) = x
 
+{-# INLINE unHCons #-}
 unHCons :: HComps (h ': hs) f a -> h (HComps hs f) a
 unHCons (HCons x) = x
 
+{-# INLINE hdecomps #-}
 hdecomps :: (HFunctor h, Functor f) => HComps '[h] f a -> h f a
 hdecomps (HCons x) = hmap unHNil x
 
+{-# INLINE hcomps #-}
 hcomps :: (HFunctor h, Functor f) => h f a -> HComps '[h] f a
 hcomps = HCons . hmap HNil
 {-
@@ -42,6 +46,7 @@ Without the `Functor (h (HComps hs f))` constraint, the following
 functor instance would not work as is:
 -}
 instance Functor f => Functor (HComps ts f) where
+  {-# INLINE fmap #-}
   fmap :: Functor f => (a -> b) -> HComps ts f a -> HComps ts f b
   fmap f (HNil x)  = HNil  (fmap f x)
   fmap f (HCons x) = HCons (fmap f x)
@@ -85,32 +90,42 @@ Using this idea, the remaining instances are fairly routine to implement.
 -}
 instance Applicative f =>
   Applicative (HComps '[] f) where
+    {-# INLINE pure #-}
     pure x = HNil (pure x)
+    {-# INLINE (<*>) #-}
     HNil mf <*> HNil mx = HNil (mf <*> mx)
 instance (Applicative f, Applicative (h (HComps hs f))) =>
   Applicative (HComps (h ': hs) f) where
+    {-# INLINE pure #-}
     pure x = HCons (pure x)
+    {-# INLINE (<*>) #-}
     HCons mf <*> HCons mx = HCons (mf <*> mx)
 
 instance Monad m =>
   Monad (HComps '[] m) where
+    {-# INLINE (>>=) #-}
     (>>=) :: Monad m => HComps '[] m a -> (a -> HComps '[] m b) -> HComps '[] m b
     HNil mx >>= f = HNil (mx >>= ((\(HNil x) -> x) . f))
 instance (Monad m, Monad (t (HComps ts m))) =>
   Monad (HComps (t ': ts) m) where
+    {-# INLINE (>>=) #-}
     (>>=) :: HComps (t : ts) m a -> (a -> HComps (t : ts) m b) -> HComps (t : ts) m b
     HCons mx >>= f = HCons (mx >>= ((\(HCons x) -> x) . f))
 
 instance HFunctor (HComps '[]) where
+  {-# INLINE hmap #-}
   hmap h (HNil x) = HNil (h x)
 instance (HFunctor h, HFunctor (HComps hs)) =>
   HFunctor (HComps (h ': hs)) where
+    {-# INLINE hmap #-}
     hmap h (HCons x) = HCons (hmap (hmap h) x)
 
 instance MonadTrans (HComps '[]) where
+  {-# INLINE lift #-}
   lift = HNil
 instance (MonadTrans t, MonadTrans (HComps ts))
   => MonadTrans (HComps (t ': ts)) where
+  {-# INLINE lift #-}
   lift x = HCons (lift (lift x))
 
 class HRecompose hs f where
@@ -118,17 +133,21 @@ class HRecompose hs f where
   hdecompose :: HComposes hs f a -> HComps hs f a
 
 instance HRecompose '[] f where
+  {-# INLINE hrecompose #-}
   hrecompose :: HComps '[] f a -> HComposes '[] f a
   hrecompose (HNil x)  = x
 
+  {-# INLINE hdecompose #-}
   hdecompose :: HComposes '[] f a -> HComps '[] f a
   hdecompose = HNil
 
 instance (Functor f, Functor (HComposes hs f), Functor (HComps hs f), HRecompose hs f, HFunctor h)
   => HRecompose (h ': hs) f where
+  {-# INLINE hrecompose #-}
   hrecompose :: HComps (h ': hs) f a -> HComposes (h ': hs) f a
   hrecompose (HCons x) = hmap hrecompose x
 
+  {-# INLINE hdecompose #-}
   hdecompose :: HComposes (h ': hs) f a -> HComps (h ': hs) f a
   hdecompose x = HCons (hmap hdecompose x)
 
@@ -137,32 +156,40 @@ class HExpose hs where
   hunexpose :: Functor f => HComps hs (HComps ks f) a -> HComps (hs :++ ks) f a
 
 instance HExpose '[] where
+  {-# INLINE hexpose #-}
   hexpose :: Functor f => HComps ks f a -> HComps '[] (HComps ks f) a
   hexpose (HNil x)  = HNil (HNil x)
   hexpose (HCons x) = HNil (HCons x)
 
+  {-# INLINE hunexpose #-}
   hunexpose :: HComps '[] (HComps ks f) a -> HComps ('[] :++ ks) f a
   hunexpose (HNil x)  = x
 
 instance (HExpose hs , HFunctor h, (forall f ks . Functor f => Functor (HComps ks f))) => HExpose (h ': hs) where
+  {-# INLINE hexpose #-}
   hexpose   (HCons x) = HCons (hmap hexpose x)
 
+  {-# INLINE hunexpose #-}
   hunexpose (HCons x) = HCons (hmap hunexpose x)
 class HExposes hs ks where
   hexposes   :: HComps (hs :++ ks) f a -> HComps hs (HComps ks f) a
   hunexposes :: HComps hs (HComps ks f) a -> HComps (hs :++ ks) f a
 
 instance HExposes '[] ks where
+  {-# INLINE hexposes #-}
   hexposes :: HComps ks f a -> HComps '[] (HComps ks f) a
   hexposes (HNil x)  = HNil (HNil x)
   hexposes (HCons x) = HNil (HCons x)
 
+  {-# INLINE hunexposes #-}
   hunexposes :: HComps '[] (HComps ks f) a -> HComps ('[] :++ ks) f a
   hunexposes (HNil x)  = x
 
 instance (HExposes hs ks, HFunctor h, (forall f ks . Functor (HComps ks f))) => HExposes (h ': hs) ks where
+  {-# INLINE hexposes #-}
   hexposes   (HCons x) = HCons (hmap hexposes x)
 
+  {-# INLINE hunexposes #-}
   hunexposes (HCons x) = HCons (hmap hunexposes x)
 
 
