e1488a205bfb2173d8c7352c4b11e767553a99aa
Nicolas Wu <nicolas.wu@gmail.com>
2025-08-04 15:34:25 +0100

    Remove TypeAbstractions

--- src/Control/Effect/CodeGen/SoPU.hs
+++ src/Control/Effect/CodeGen/SoPU.hs
@@ -30,7 +30,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 -}
 
 {-# LANGUAGE TypeFamilies, UnicodeSyntax, TemplateHaskell, BlockArguments #-}
-{-# LANGUAGE UndecidableInstances, TypeAbstractions #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 
 
